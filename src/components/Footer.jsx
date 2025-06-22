4a97935e59750d5d968a794b13228c171ea0fd52
Myat Mon <ayemyatmon.yn@gmail.com>
2025-06-22 15:25:07 +0100

    added scroll to top button

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Section from "./Section";
+import ScrollToTop from "./ScrollToTop";
 
 export default function Footer() {
   return (
     <div className="bg-green-700">
+      <ScrollToTop />
       <div className="container mx-auto px-4 py-8 text-center text-white">
         © 2025 Mon. Learned React.
       </div>
