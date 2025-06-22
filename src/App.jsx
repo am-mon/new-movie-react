165e1ccaca83b465a93657cc0deea5f3e17be75d
Myat Mon <ayemyatmon.yn@gmail.com>
2025-06-22 17:36:06 +0100

    Added GA4

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import ReactGA from "react-ga4";
+
+import MainLayout from "./layouts/MainLayout";
 import Home from "./pages/Home";
 import MovieDetailPage from "./pages/MovieDetailPage";
-import MainLayout from "./layouts/MainLayout";
 import CastDetailPage from "./pages/CastDetailPage";
 import SearchResults from "./pages/SearchResults";
 
+const TRACKING_ID = "G-015QD939P7";
+
+function AnalyticsTracker() {
+  const location = useLocation();
+
+  useEffect(() => {
+    ReactGA.send({
+      hitType: "pageview",
+      page: location.pathname + location.search,
+    });
+  }, [location]);
+
+  return null;
+}
+
 export default function App() {
+  useEffect(() => {
+    ReactGA.initialize(TRACKING_ID);
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
